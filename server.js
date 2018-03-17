2d73251ea429832faaa2dfcf194fc59f7fe05bdc
Vajinder Gill <v.gill@se15.qmul.ac.uk>
2018-03-17 21:19:47 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs'); // setting the view engine to handlebars
+
+app.use(function(request, response, next)
+{
+  var now = new Date().toString();
+  var log = `${now}: ${request.method} ${request.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', function(error)
+  {
+    if(error)
+    {
+      console.log('Unable to append to the file.');
+    }
+  });
+
+  next();
+});
+
+// app.use(function(request, response, next)
+// {
+//   response.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', function()
+{
+  return new Date().getFullYear();
+}); //Helpers are like Partials but they're usually just for FUNCTIONS,
+   // not specific files of code!!
+
+hbs.registerHelper('screamIt', function(text)
+{
+  return text.toUpperCase();
+});
+
+app.get('/', function(request, response)
+{
+  response.render('home.hbs', {
+    pageTitle: 'Homepage',
+    welcomeMessage: 'Welcome to my Homepage!',
+  });
+});
+
+app.get('/about', function(request, response)
+{
+  response.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', function(request, response)
+{
+  response.send({
+    errorMessage: 'Unable to fulfil this request'
+  })
+})
+
+app.listen(3000, function()
+{
+  console.log('Server is up on port 3000.');
+});
